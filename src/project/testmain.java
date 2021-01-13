6dba8e983487d7866904309a4356ac4139af842e
woals7819 <77331482+woals7819@users.noreply.github.com>
2021-01-13 23:02:39 +0900

    Update testmain.java

--- src/project/testmain.java
+++ src/project/testmain.java
@@ -5,25 +5,43 @@ import java.util.Scanner;
 public class testmain {
 
 	public static void main(String[] args) {
-		String[] ms = new String[100];
-		int count =0;
+		String[][] ms2 = new String[100][];
+
+		int count = 0;
+		int seq = 1;
+
 		Scanner sc = new Scanner(System.in);
-		while(true) {
+
+		while (true) {
 			System.out.println("명령어를 입력해 주세요. :");
 			String a = sc.next();
-			
-			if(a.equals("add")) {
-				System.out.println("게시물의 제목을 입력해 주세요. :");
-				String wpahr = sc.nextLine();
-				System.out.println("게시물의 내용을 입력해 주세요. :");
-				String sodyd = sc.nextLine();
-				System.out.println();
-					
-				}
+
+			if (a.equals("add")) {
+				String[] ms = new String[3];
+				ms[0] = String.valueOf(seq);
+
+				System.out.println(ms[0] + "게시물의 제목을 입력해 주세요. :");
+				ms[1] = sc.next();
+
+				System.out.println("--------------------------");
+
+				System.out.println(ms[0] + "게시물의 내용을 입력해 주세요. :");
+				ms[2] = sc.next();
+
+				ms2[count] = ms;
+				System.out.println("게시물이 등록 되었습니다.");
+
+				seq++;
+				count++;
 			}
 			
-			if(a.equals("list")) {
-				
+			//리스트 입력 시, 전체 리스트가 안나옴
+			else if (a.equals("list")) {
+				for (int i = 0; i < count; i++) {
+					System.out.println(
+							ms2[i][0] + "제목 [ " + ms2[i][1] + "]" + "->" + ms2[i][0] + "내용[" + ms2[i][2] + "]");
+				}
+
 			}
 			
 			if(a.equals("exit")) {
