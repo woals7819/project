402e77a9c33bf43ffa7874b69add03ae8c9194ee
woals7819 <woals7819@naver.com>
2021-01-19 21:56:38 +0900

    20210119

--- src/project/exm2.java
+++ src/project/exm2.java
@@ -7,6 +7,8 @@ public class exm2 {
 	public static void main(String[] args) {
 		ArrayList<Integer> a = new ArrayList<>();
 		ArrayList<String> b = new ArrayList<>();
+		String[][] zv = new String[6][2];
+		int count  = 0;
 		
 		parson 사람1 = new parson(17, "홍길동");
 	    parson 사람2 = new parson(19, "임꺽정");
@@ -15,6 +17,12 @@ public class exm2 {
 	    parson 사람5 = new parson(53, "을지문덕");
 	    parson 사람6 = new parson(12, "황진이");
 	    parson av = new parson();
+	    
+	    for(int i= 0; i < zv.length; i++) {
+//	    	zv[count][0] = a.get(i);
+	    	zv[count][1] = b.get(i);
+	    }
+	    
 	    
 	    a.add(사람1.age);
 	    b.add(사람1.name);
@@ -29,8 +37,13 @@ public class exm2 {
 	    a.add(사람6.age);
 	    b.add(사람6.name);
 	    
+	    
+	    
+	    
+	    
 	    for(int i = 0; i < b.size(); i++) {
-//	    	av.ghaud(b.get(i));
+	    	zv[count][0] = a.get(i);
+
 	    	
 	    }
 	    
--- src/project/homePage.java
+++ src/project/homePage.java
@@ -6,40 +6,99 @@ import java.util.Scanner;
 public class homePage {
 
 	public static void main(String[] args) {
-		ArrayList<String> title = new ArrayList<String>(); //제목리스트
-		ArrayList<String> contents = new ArrayList<String>(); //내용리스트
 		ArrayList<Integer> postNumber = new ArrayList<Integer>();
-		
-		Scanner ms = new Scanner(System.in);
-		int count = 0; //게시물 개수
-		int esq = 1; //게시물 번호
+		ArrayList<String> title = new ArrayList<String>(); // 제목리스트
+		ArrayList<String> contents = new ArrayList<String>(); // 내용리스트
 
-		while(true) {
+		Scanner ms = new Scanner(System.in);
+		int count = 0; // 게시물 개수
+		int esq = 1; // 게시물 번호
+
+		while (true) {
 			System.out.println("명령어를 입력해주세요.:");
 			String command = ms.next();
-			
-			if(command.equals("add")) {
+
+			if (command.equals("add")) {
 				postNumber.add(esq);
 				System.out.println("제목을 입력해주세요.");
 				title.add(count, ms.next());
 				System.out.println("내용을 입력해주세요.");
 				contents.add(count, ms.next());
-				
+
 				count++;
 				esq++;
 			}
-			
-			else if(command.equals("list")) {
-				for(int i= 0; i < count; i++) {
+
+			else if (command.equals("list")) {
+				for (int i = 0; i < count; i++) {
 					System.out.println(postNumber.get(i) + "번 제목:" + title.get(i));
 					System.out.println(postNumber.get(i) + "번 내용:" + contents.get(i));
 				}
 			}
-			
-			else if(command.equals("exit")) {
+
+			else if (command.equals("update")) {
+
+				System.out.println("수정할 게시물의 번호를 입력하세요. :");
+				int up = ms.nextInt();
+				int qlry = -1;
+
+				for (int i = 0; i < postNumber.size(); i++) {
+					int updateNumber = postNumber.get(i);
+					if (updateNumber == up) {
+						qlry= i;
+					}
+					
+					if(qlry != -1) {
+						System.out.println("제목 :");
+						String newtitle = ms.next();
+						title.set(qlry, newtitle);
+						System.out.println("내용 :");
+						String newContents = ms.next();
+						contents.set(qlry, newContents);
+						
+						System.out.println("수정이 완료되었습니다.");
+						
+						for (int z = 0; z < count; z++) {
+							System.out.println(postNumber.get(z) + "번 제목:" + title.get(z));
+							System.out.println(postNumber.get(z) + "번 내용:" + contents.get(z));
+						}
+						
+					}
+					else {
+						System.out.println("없는 게시물 번호 입니다.");
+					}
+				}
+			}
+
+			else if (command.equals("delet")) {
+				System.out.println("삭제할 게시물의 번호를 입력해주세요. :");
+				int delet = ms.nextInt();
+				int qlry = -1;
+
+				for (int i = 0; i < postNumber.size(); i++) {
+					int deletPostNuber = postNumber.get(i);
+					if (delet == deletPostNuber) {
+						qlry = i;
+
+					} 
+					
+					if(qlry != -1) {
+						postNumber.remove(qlry);
+						title.remove(qlry);
+						contents.remove(qlry);
+						
+						System.out.println("삭제가 완료되었습니다.");					
+					}
+					else {
+						System.out.println("없는 게시물 번호 입니다.");
+					}
+				}
+			}
+
+			else if (command.equals("exit")) {
 				break;
 			}
-			
+
 		}
 
 	}
